06b897c02dc5035cba3652226c74e299c5b8855e
johanananana <johanaylen219@gmail.com>
2025-11-06 01:06:01 -0300

    nvgf

--- src/Main.java
+++ src/Main.java
@@ -4,5 +4,9 @@ public class Main {
     public static void main(String[] args) {
 
         System.out.println("holiwis");
+        System.out.println("holiwis");
+        System.out.println("holiwis");
+        System.out.println("holiwis");
+        System.out.println("holiwis");
         }
     }
