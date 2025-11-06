5595e0e9637e6a1e7b4672c1fa9c133218de7688
johanananana <johanaylen219@gmail.com>
2025-11-06 06:28:21 -0300

    consulta
    fijense la clase cine

--- src/Models/Cine.java
+++ src/Models/Cine.java
@@ -9,5 +9,6 @@ public class Cine
     private String nombreCine;
     private RepositorioCliente cliente;
     private HashMap<Integer, Sala> funcionesPorSala;
+    private String archivoJsoncine;
     /// a que se refiere con archivojsoncine de tipo string???
 }
