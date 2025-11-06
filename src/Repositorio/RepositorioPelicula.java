04d525fbeaa5a8a47dd582e467a77704f6f35b88
AgostinaMartinez19 <agostinamartinezz1988@gmail.com>
2025-11-06 06:13:02 -0300

    hice repositorioPeli

--- src/Repositorio/RepositorioPelicula.java
+++ src/Repositorio/RepositorioPelicula.java
@@ -1,5 +1,39 @@
 package Repositorio;
 
-public class RepositorioPelicula {
+import Contenedoras.GestionDeElementos;
+import Excepciones.ElementoNoExiste;
+import Excepciones.ElementoRepetido;
+import Excepciones.VerificarNulo;
+import Models.Pelicula;
+import org.json.JSONArray;
 
+public class RepositorioPelicula {
+    private GestionDeElementos<Pelicula>peliculaGestionDeElementos;
+    private JSONArray j;
+    public RepositorioPelicula() {
+        this.peliculaGestionDeElementos = new GestionDeElementos<>();
+        this.j = new JSONArray();
+    }
+
+    public boolean agregarPelicula ( Pelicula p ) throws VerificarNulo, ElementoRepetido {
+        return this.peliculaGestionDeElementos.agregarElemento(p);
+    }
+
+    public boolean EliminarPelicula ( int id) throws  VerificarNulo, ElementoRepetido, ElementoNoExiste {
+        return peliculaGestionDeElementos.eliminarElemento(id);
+    }
+    public Pelicula buscarElemento(int id) throws ElementoNoExiste, VerificarNulo, ElementoRepetido{
+        return peliculaGestionDeElementos.buscarElemento(id);
+    }
+
+    public JSONArray toJson() {
+        JSONArray jsonArray = new JSONArray();
+
+        for (Pelicula pelicula : this.peliculaGestionDeElementos.getElementos()) { // Recorre una por una todas las películas que están guardadas dentro de tu GestionDeElementos.
+            jsonArray.put(pelicula.toJson());
+        }
+
+        return jsonArray; // devuelve arreglo JSON
+    }
+    
 }
